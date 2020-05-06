11ed68194a24f5672b1745563e3e36d79e157c92
Kevin Mathias <komathias1@gmail.com>
2020-05-06 23:15:27 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+ has_many :characters
+ has_many :shows
+ 
+ def full_name
+   return "#{self.first_name}" + " #{self.last_name}"
+ end
+ 
+ def list_roles
+    self.characters.collect do |character|
+     "#{character.name} - #{character.show.name}"
+    end.join
+  end
+ 
+ 
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,14 @@
 class Character < ActiveRecord::Base
+  belongs_to :actor
+  belongs_to :show
+  attr_accessor :catchphrase
+  
+  def say_that_thing_you_say
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+  
+  def catchphrase
+    
+  end
   
 end
