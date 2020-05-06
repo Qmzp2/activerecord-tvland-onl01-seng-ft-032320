1d5a8b7d00eafbc798ca5d398fe096ea37edf164
Kevin Mathias <komathias1@gmail.com>
2020-05-06 23:15:47 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_character_catchphrase.rb
+++ db/migrate/006_add_character_catchphrase.rb
@@ -1,5 +1,5 @@
 
-class AddCharacterCatchphrase < ActiveRecord::Migration[5.2]
+class AddCharacterCatchphrase < ActiveRecord::Migration[5.1]
   def change
     add_column :characters, :catchphrase, :string 
   end
